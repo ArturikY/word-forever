b7a16401123eb94a9444db77a23a231861acc854
lapyxxx <enigartsummer@gmail.com>
2024-04-17 17:24:20 +0300

    v1

--- js/script.js
+++ js/script.js
@@ -23,8 +23,10 @@ $(function () {
   $(selectAllItems).click(function () {
     if ($(this).prop("checked")) {
       $(checkboxItem).prop("checked", true);
+      document.querySelector(".btn-add").disabled = false;
     } else {
       $(checkboxItem).prop("checked", false);
+      document.querySelector(".btn-add").disabled = true;
     }
   });
 
@@ -94,7 +96,9 @@ $(document).ready(function () {
       email: "Пожалуйста, укажите email или телефон",
     },
     submitHandler: function (form) {
-      document.querySelector(".login-modal").addEventListener("submit", (e) => checkLogin(e));
+      document
+        .querySelector(".login-modal")
+        .addEventListener("submit", (e) => checkLogin(e));
     },
   });
 
@@ -119,7 +123,9 @@ $(document).ready(function () {
       email: "Пожалуйста, укажите email или телефон",
     },
     submitHandler: function (form) {
-      document.querySelector(".reg-modal").addEventListener("submit", (e) => checkLogin(e));
+      document
+        .querySelector(".reg-modal")
+        .addEventListener("submit", (e) => checkLogin(e));
     },
   });
 
@@ -127,7 +133,9 @@ $(document).ready(function () {
   $("#sign_in_btn").click(function () {
     // Вызов функции проверки формы
     if (validateForm()) {
-      document.querySelector(".reg-modal").addEventListener("submit", (e) => checkLogin(e));
+      document
+        .querySelector(".reg-modal")
+        .addEventListener("submit", (e) => checkLogin(e));
     }
   });
 
@@ -219,6 +227,21 @@ $(document).ready(function () {
   //     /////
   //   }
   // };
+  let cards = [
+    {
+      number: 0,
+      words: [],
+    },
+  ];
+  let words = [
+    { word: "Азбука", blur: "03" },
+    { word: "Игра", blur: "0" },
+    { word: "Полтинник", blur: "1" },
+    { word: "Жизнь", blur: "1" },
+    { word: "Мечта", blur: "1" },
+    { word: "сорок", blur: "1" },
+  ];  
+  let filteredWords = Object.assign([], words);
 
   async function checkLogin(e) {
     try {
@@ -228,22 +251,209 @@ $(document).ready(function () {
       let formData = new FormData(e.target);
       const formDataObj = Object.fromEntries(formData.entries());
 
-      console.log(formDataObj);
       const response = await fetch(url, {
         method: "POST",
         mode: "no-cors",
         body: JSON.stringify(formData),
         headers: {
           "Content-type": "application/json; charset=UTF-8",
-          "Access-Control-Allow-Origin": "*"
+          "Access-Control-Allow-Origin": "*",
         },
       });
 
       const data = await response.json();
-      console.log(data);
       e.target.reset();
     } catch (e) {
       console.error(e);
     }
+  }
+  const mainSideBtn = document.querySelector(".nav-link-cards");
+  if (mainSideBtn) {
+    mainSideBtn.addEventListener("click", () => {
+      if (mainSideBtn.classList.contains("activity")) {
+        window.location.href = "/personal-cabinet.html";
+      }
+      // mainSideBtn.classList.toggle('activity')
+    });
+    if (window.innerWidth < 800) {
+      document.getElementById("v-pills-home-tab").classList.remove("active");
+      document.getElementById("v-pills-home-tab").classList.remove("activity");
+      document.getElementById("v-pills-home").classList.remove("active");
+      document.getElementById("v-pills-home").classList.remove("show");
+    }
+    document
+      .querySelector(".nav-pills")
+      .querySelectorAll(".nav-link")
+      .forEach((btn) => {
+        btn.addEventListener("click", () => {
+          document
+            .querySelector(".nav-pills")
+            .querySelectorAll(".nav-link")
+            .forEach((btn) => {
+              btn.classList.remove("activity");
+            });
+          btn.classList.add("activity");
+        });
+      });
+  }
+  deleteProfileBtn = document.querySelector(".confirm-delete-btn");
+  saveData = document.querySelector(".save-btn");
+  if (deleteProfileBtn) {
+    deleteProfileBtn.addEventListener("click", () => {
+      deleteProfile();
+    });
+    saveData.addEventListener("click", () => {
+      editProfileData();
+    });
+  }
+
+  async function editProfileData() {
+    try {
+      personalData = new FormData(document.querySelector(".editProfileData"));
+      const personalDataObj = Object.fromEntries(personalData.entries());
+      editProfileUrl = "http://localhost:5500/api/auth/register";
+      const response = await fetch(editProfileUrl, {
+        method: "PATCH",
+        body: JSON.stringify(personalDataObj),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+          "Access-Control-Allow-Origin": "*",
+        },
+      });
+      const data = await response.json();
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  async function deleteProfile() {
+    try {
+      deleteProfileUrl = "http://localhost:5500/api/auth/register";
+      const response = await fetch(deleteProfileUrl, {
+        method: "DELETE",
+        body: "delete",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+          "Access-Control-Allow-Origin": "*",
+        },
+      });
+      const data = await response.json();
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  const addinglist = document.querySelector(".adding-list");
+  if (addinglist) {
+    document.querySelector(".btn-add").addEventListener("click", () => {
+      createNewCard();
+    });
+    async function createNewCard() {
+      try {
+        newCardUrl = "http://localhost:5500/api/auth/register";
+        const response = await fetch(newCardUrl, {
+          method: "POST",
+          body: JSON.stringify(addedWords),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+            "Access-Control-Allow-Origin": "*",
+          },
+        });
+        const data = await response.json();
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
+    //активирую поиск
+    const search = document.querySelector(".search");
+    search.oninput = (e) => {
+      // resetWords();
+      checkSearch(e);
+      hideUnusedLetter();
+      console.log(addedWords);
+    };
+    //функция поиска
+    const resetWords = () => {
+      addinglist.querySelectorAll(".adding-item").forEach((item) => {
+        const wordsContainer = item.querySelector(".words");
+        wordsContainer.innerHTML = "";
+      });
+    };
+    const checkSearch = (e) => {
+      if (e.target.value) {
+        let searchValue = e.target.value.trim().toLowerCase();
+        addinglist.querySelectorAll("label").forEach((label) => {
+          label.classList.remove("hide");
+          if (
+            label
+              .querySelector("span")
+              .textContent.toLowerCase()
+              .search(searchValue) === -1
+          ) {
+            label.classList.add("hide");
+          }
+        });
+
+        // filteredWords = Object.assign([], words).filter(curr => {
+        //   if (curr.word.toLowerCase().search(searchValue) !== -1) {
+        //     return true
+        //   }
+        // })
+      } else {
+        addinglist.querySelectorAll("label").forEach((label) => {
+          label.classList.remove("hide");
+        });
+      }
+    };
+
+    //прячу все буквы, где нет слов
+    addedWords = [];
+    const hideUnusedLetter = () => {
+      addinglist.querySelectorAll(".adding-item").forEach((item) => {
+        flag = true;
+        item.querySelectorAll("label").forEach((label) => {
+          if (!label.classList.contains("hide")) {
+            flag = false;
+          }
+        });
+        if (flag) {
+          item.classList.add("hide");
+        } else {
+          item.classList.remove("hide");
+        }
+      });
+    };
+
+    //вывод отфильтрованных слов
+    const showFilteredWords = () => {
+      filteredWords.forEach((word) => {
+        const letter = word.word.toLowerCase().slice(0, 1);
+        addinglist.querySelectorAll(".adding-item").forEach((item) => {
+          if (letter === item.querySelector("p").textContent.slice(1, 2)) {
+            let label = document.createElement("label");
+            label.innerHTML = `<input type="checkbox" class="word-cb" value="${word.word}"><span>${word.word}</span>`;
+            item.querySelector(".words").appendChild(label);
+            item.classList.remove("hide");
+          }
+        });
+      });
+    };
+
+    showFilteredWords();
+    hideUnusedLetter();
+    //добавление/удаление списка слов новой карточки, отключение кнопки
+    addinglist.querySelectorAll(".word-cb").forEach((word) => {
+      word.addEventListener("click", () => {
+        if (word.checked) {
+          addedWords.push(word.value);
+          document.querySelector(".btn-add").disabled = false;
+        } else {
+          addedWords.splice(addedWords.indexOf(word.value), 1);
+          if (addedWords.length == 0) {
+            document.querySelector(".btn-add").disabled = true;
+          }
+        }
+        console.log(addedWords);
+      });
+    });
   }
 });
